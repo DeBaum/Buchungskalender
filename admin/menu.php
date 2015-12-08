caddc7f328656464593fd3b7625b9a81f448c28d
Dustin Baum <dustin.baum@gmail.com>
2015-12-08 17:40:42 +0100

    edit admin menu slugs

--- admin/menu.php
+++ admin/menu.php
@@ -3,14 +3,14 @@
 add_action('admin_menu', 'createPluginAdminMenu');
 
 function createPluginAdminMenu() {
-	$menuSlug = 'buchungskalender_menu';
+	$menuSlug = 'buchungskalender';
 	$menuTitle = 'Buchungskalender';
 
 	add_menu_page($menuTitle, $menuTitle, 'manage_options', $menuSlug, 'adminMenu_index', 'dashicons-calendar-alt');
 	add_submenu_page($menuSlug, $menuTitle, 'Einstellungen', 'manage_options', $menuSlug, 'adminMenu_index');
-	add_submenu_page($menuSlug, 'Kategorien - ' . $menuTitle, 'Kategorien', 'manage_options', $menuSlug . '_categories', 'adminMenu_categories');
-	add_submenu_page($menuSlug, 'Objekte - ' . $menuTitle, 'Objekte', 'manage_options', $menuSlug . '_objects', 'adminMenu_objects');
-	add_submenu_page($menuSlug, 'Extras - ' . $menuTitle, 'Extras', 'manage_options', $menuSlug . '_extras', 'adminMenu_extras');
+	add_submenu_page($menuSlug, 'Kategorien - ' . $menuTitle, 'Kategorien', 'manage_options', $menuSlug . '-categories', 'adminMenu_categories');
+	add_submenu_page($menuSlug, 'Objekte - ' . $menuTitle, 'Objekte', 'manage_options', $menuSlug . '-objects', 'adminMenu_objects');
+	add_submenu_page($menuSlug, 'Extras - ' . $menuTitle, 'Extras', 'manage_options', $menuSlug . '-extras', 'adminMenu_extras');
 }
 
 function adminMenu_index() {

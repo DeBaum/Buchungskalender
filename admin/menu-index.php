44c4cfbbeff83f434c8ac35017b99c2220bbe678
Dustin Baum <dustin.baum@gmail.com>
2015-12-09 11:52:12 +0100

    added adminMenu index page links

--- admin/menu-index.php
+++ admin/menu-index.php
@@ -1,3 +1,9 @@
 <div class="wrap">
 	<h1>Buchungskalender</h1>
+	
+	<ul>
+		<li><a href="<?php menu_page_url('buchungskalender-categories') ?>">Kategorien</a></li>
+		<li><a href="<?php menu_page_url('buchungskalender-objects') ?>">Objekte</a></li>
+		<li><a href="<?php menu_page_url('buchungskalender-extras') ?>">Extras</a></li>
+	</ul>
 </div>

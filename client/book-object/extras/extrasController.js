7faf51957570940c01d8788042a4a27c02a65dfb
baba43 <basti743@gmail.com>
2016-01-06 16:51:58 +0100

    Buchungsseite unterstützt jetzt alle Extras

--- client/book-object/extras/extrasController.js
+++ client/book-object/extras/extrasController.js
@@ -12,8 +12,9 @@
                 "type": "select",
                 "name": "sitzordung",
                 "values": [
+                    {"id": 0, "title": "Keine", "default": true},
                     {"id": 1, "title": "U-Form"},
-                    {"id": 2, "title": "T-Form", "default": true}
+                    {"id": 2, "title": "T-Form"}
                 ]
             },
             {
@@ -30,6 +31,15 @@
                 "title": "Anzeige Infotafel",
                 "type": "check",
                 "name": "infotafel"
+            },
+            {
+                "id": 13,
+                "title": "Eventzeit",
+                "type": "form",
+                "subfields": [
+                    {"type": "text", "title": "Start", "name": "time_start"},
+                    {"type": "text", "title": "Ende", "name": "Time_stop"}
+                ]
             }
         ];
 
@@ -37,17 +47,18 @@
         vm.getFields = getFields;
         vm.getForms = getForms;
         vm.getSelectDefault = getSelectDefault;
+        vm.getAll = getAll;
 
         ////////////
 
         function getFields() {
-            return _.filter(list, function(e) {
+            return _.filter(list, function (e) {
                 return e.type != 'form';
             });
         }
 
         function getForms() {
-            return _.filter(list, function(e) {
+            return _.filter(list, function (e) {
                 return e.type == 'form';
             });
         }
@@ -57,6 +68,10 @@
                 return _.first(_.find(extra.values, {"default": true}));
             }
             return null;
+        }
+
+        function getAll() {
+            return list;
         }
     }
 })();

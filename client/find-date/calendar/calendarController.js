9a8bfb13f8594cd83d764b831a1c2a4c09cbfc1d
baba43 <basti743@gmail.com>
2016-01-06 15:16:46 +0100

    TODO hinzugefügt

--- client/find-date/calendar/calendarController.js
+++ client/find-date/calendar/calendarController.js
@@ -8,7 +8,7 @@
         var d = date.getDate();
         var m = date.getMonth();
         var y = date.getFullYear();
-        var allEvents = [
+        var allEvents = [ // TODO: Demodaten entfernen?
             {title: 'All Day Event', start: new Date(y, m, 1)},
             {title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2)},
             {id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false},

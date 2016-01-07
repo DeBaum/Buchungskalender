0ded68a12cb07f17a69a633024f1400055f8dc72
Dustin Baum <dustin.baum@gmail.com>
2016-01-07 12:03:53 +0100

    local images for categorys

--- client/categoryService.js
+++ client/categoryService.js
@@ -10,17 +10,17 @@
                 {
                     id: 1,
                     title: 'Autos',
-                    thumbnail: '//upload.wikimedia.org/wikipedia/commons/9/95/Ferrari_P4-5.jpg'
+                    thumbnail: '//localhost.de/static/auto.jpg'
                 },
                 {
                     id: 2,
                     title: 'Räume',
-                    thumbnail: '//upload.wikimedia.org/wikipedia/commons/d/df/Ravensburg_Mevlana-Moschee_Schulungsraum.jpg'
+                    thumbnail: '//localhost.de/static/raum.jpg'
                 },
                 {
                     id: 3,
                     title: 'Beamer',
-                    thumbnail: '//pixabay.com/static/uploads/photo/2013/10/17/14/15/beamer-196951_960_720.jpg'
+                    thumbnail: '//localhost.de/static/beamer.jpg'
                 }
             ]
         };
